efe0738f24954f9d581acc97c46edf3ab6665b71
Rahul Lal <Rahul21Lal@gmail.com>
2019-03-18 22:09:51 +1300

    More Copying
    Downloaded all content, continue to tidy up.

---
+++ js/script.js
@@ -0,0 +1,35 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n)
+{
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n)
+{
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n)
+{
+    var i = 0; 
+
+  var slides = document.getElementsByClassName("fadingSlide");
+
+  if (n > slides.length)
+  {
+      slideIndex = 1;
+  }
+  else if (n < 1)
+  {
+      slideIndex = slides.length;
+  }
+  
+  for (; i < slides.length; i++)
+  {
+      slides[i].style.display = "none"; 
+  }
+
+  slides[slideIndex-1].style.display = "block";
+}
